d74ac93d13e6ad2b8c0cdc7220734f3aeff36175
martenLaasi <144745408+martenLaasi@users.noreply.github.com>
2023-10-31 17:05:07 +0000

    Create recapCode.py
    code was written by students who controlled an AI teacher.

---
+++ recapCode.py
@@ -0,0 +1,38 @@
+#from Constant import *
+#num = 1
+#greeting = "Hello world"
+#prints hello world on the console
+#print(greeting, num + 2)
+#print(type(num))
+#print(type(greeting
+#x = 1
+#def kys():
+   # global x
+  #  x = "diabetesfrenchisdumbbangladeshisevendumber"
+    
+ #   print(x)
+
+ 
+#kys()
+#print(x)
+
+
+#print(PI)
+
+
+
+
+#array=[420,69,911,2001]
+#print(array[1])
+#inputs=int(input("input a number"))
+#for i in array:
+  #  if inputs==array[i]:
+       # print("its therebbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+
+thisdict = {
+    "Brand" : "car",
+    "model" : "4 wheels",
+    "YEI" : "963"
+    }
+
+print(thisdict)
